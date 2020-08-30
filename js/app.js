929dbdd4a07d8dd42671130942c805b58b20bcc4
Evan Motte <evanwilliammotte@gmail.com>
2020-08-30 17:12:58 -0400

    bugfixes

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
 $(document).foundation()
 
+$(".myMealType").on("click", function() {   
+  event.preventDefault()   
+  var mealTypeChoice = this.value.trim()   
+  var typeChoiceSearch = mealTypeChoice.replace(/\s+/g, '').toLowerCase()   
+  console.log(mealTypeChoice)   
+  $(".typeSelector").text(mealTypeChoice)   
+  $(".typeSelector").addClass("success")
+
 
 $(".searchBtn").on("click", function () {
   event.preventDefault()
   var cuisine = $(".cuisine").val().trim()
   var cravings = $(".cravings").val().trim()
-  var maxCalories = $("#sliderOutput2").val()
+  var maxCalories = $("#sliderOutput1").val()
   var maxCarbs = $("#sliderOutput2").val()
   var maxProtein = $("#sliderOutput3").val()
   console.log(cuisine)
   console.log(cravings)
 
 
-  var queryURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + cravings + "&cuisine=" + cuisine + "&maxCalories=" + maxCalories + "&maxCarbs=" + maxCarbs + "&maxProtein=" + maxProtein + "&addRecipeInformation=true&sort=random&apiKey=e703032e7e094173b8b055bd1956b250"
+  var queryURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + cravings + "&cuisine=" + cuisine + "&type=" + typeChoiceSearch + "&maxCalories=" + maxCalories + "&maxCarbs=" + maxCarbs + "&maxProtein=" + maxProtein + "&addRecipeInformation=true&sort=random&apiKey=5accba34b6cd4c4cbe664a6e1e39df48"
 
   $.ajax({
     url: queryURL,
@@ -24,36 +32,20 @@ $(".searchBtn").on("click", function () {
       var foodID = response.results[i].id
       var steak = response.results[i].title
       var image = response.results[i].image
+      var servingSize = response.results[i].servings
+      var servingCal = (response.results[i].nutrition[0].amount).toFixed(0)
+      var servingCar = (response.results[i].nutrition[2].amount).toFixed(0)
+      var servingPro = (response.results[i].nutrition[1].amount).toFixed(0)
       $(`.title${i}`).text(steak)
       $(`.img${i}`).attr("src", `${image}`)
+      $(`.recipe${i}`).append(`<h1 class="modalText">Total Servings: ${servingSize}</h1>`)
+      $(`.recipe${i}`).append(`<h2 class="modalText ${i}a" calNum${i}="${servingCal}">Calories per Serving: ${servingCal}</h2>`)
+      $(`.recipe${i}`).append(`<h3 class="modalText ${i}b" carNum${i}="${servingCar}">Carbs per Serving: ${servingCar}</h3>`)
+      $(`.recipe${i}`).append(`<h4 class="modalText ${i}c" proNum${i}="${servingPro}">Protein per Serving: ${servingPro}</h4>`)
     }
-
-    
-    ingredientURL = "https://api.spoonacular.com/recipes/" + foodID + "/ingredientWidget.json?apiKey=e703032e7e094173b8b055bd1956b250"
-
-    $.ajax({
-      url: ingredientURL,
-      method: "GET"
-    }).then(function (response) {
-      $(".name").text(response.ingredients[0].name)
-      $(".inimg").attr("src", `https://spoonacular.com/cdn/ingredients_100x100/` + `${response.ingredients[0].image}`)
-      console.log(response)
-
-    })
-    recipeURL = "https://api.spoonacular.com/recipes/" + foodID + "/analyzedInstructions&apiKey=e703032e7e094173b8b055bd1956b250"
-
-
-    $.ajax({
-      url: recipeURL,
-      method: "GET"
-    }).then(function (response) {
-
-    })
-    
-
   })
-}
-)
+})
+})
 
 
 
--- js/calculator.js
+++ js/calculator.js
@@ -50,6 +50,39 @@ function progressBars() {
     } else {
         $(".bar6").width(((progressPro / totalPro).toFixed(2) * 100) + "%")
     }
+
+    var calColor = (parseInt(progressCal) / parseInt(totalCal)).toFixed(2)
+    if (calColor <= .5) {
+        $(".bar1").addClass("success")
+    } else if (calColor >.5 && calColor<= .75) {
+        $(".bar1").removeClass("success")
+        $(".bar1").addClass("warning")
+    } else if (calColor >.75 && calColor<= 1) {
+        $(".bar1").removeClass("warning")
+        $(".bar1").addClass("alert")
+    }
+
+    var carColor = (parseInt(progressCar) / parseInt(totalCar)).toFixed(2)
+    if (carColor <= .5) {
+        $(".bar2").addClass("success")
+    } else if (carColor >.5 && carColor<= .75) {
+        $(".bar2").removeClass("success")
+        $(".bar2").addClass("warning")
+    } else if (carColor >.75 && carColor<= 1) {
+        $(".bar2").removeClass("warning")
+        $(".bar2").addClass("alert")
+    }
+
+    var proColor = (parseInt(progressPro) / parseInt(totalPro)).toFixed(2)
+    if (proColor <= .5) {
+        $(".bar3").addClass("alert")
+    } else if (proColor >.5 && proColor<= .75) {
+        $(".bar3").removeClass("alert")
+        $(".bar3").addClass("warning")
+    } else if (proColor >.75 && proColor<= 1) {
+        $(".bar3").removeClass("warning")
+        $(".bar3").addClass("success")
+    }
 }
 
 $(".updateBtn1").on("click", function() {
@@ -65,5 +98,122 @@ $(".updateBtn1").on("click", function() {
     progressBars()
    
 })
+
+$(".recipeUpdateBtn1").on("click", function() {
+    var servingsEaten = parseInt($(".servingsEaten").val())
+    var pullCal = $(".0a").attr("calNum0")
+    var calculateCal = (servingsEaten * pullCal)
+    var pullCar = $(".0b").attr("carNum0")
+    var calculateCar = (servingsEaten * pullCar)
+    var pullPro = $(".0c").attr("proNum0")
+    var calculatePro = (servingsEaten * pullPro)
+    $(".updateModal1").empty()
+    $(".updateModal1").append(`<label class="caloriesToUpdate">Calories to update: ${calculateCal}</label>`)
+    $(".updateModal1").append(`<label class="carbsToUpdate">Carbs to update: ${calculateCar}</label>`)
+    $(".updateModal1").append(`<label class="proteinToUpdate">Protein to update: ${calculatePro}</label>`)
+    $(".updateModal1").append(`<button class="finalUpdate success button" data-close type="button">Submit Changes!</button>`)
+    
+    var updatedCal = parseInt($(".bar1").attr(`aria-valuenow`)) + parseInt(calculateCal)
+    $(".bar1").attr(`aria-valuenow`, updatedCal)
+    var updatedCar = parseInt($(".bar2").attr(`aria-valuenow`)) + parseInt(calculateCar)
+    $(".bar2").attr(`aria-valuenow`, updatedCar)
+    var updatedPro = parseInt($(".bar3").attr(`aria-valuenow`)) + parseInt(calculatePro)
+    $(".bar3").attr(`aria-valuenow`, updatedPro)
+    progressBars()
+
+})
+
+$(".recipeUpdateBtn2").on("click", function() {
+    var servingsEaten = parseInt($(".servingsEaten2").val())
+    var pullCal2 = $(".1a").attr("calNum1")
+    var calculateCal = (servingsEaten * pullCal2)
+    var pullCar2 = $(".1b").attr("carNum1")
+    var calculateCar = (servingsEaten * pullCar2)
+    var pullPro2 = $(".1c").attr("proNum1")
+    var calculatePro = (servingsEaten * pullPro2)
+    $(".updateModal2").empty()
+    $(".updateModal2").append(`<label class="caloriesToUpdate">Calories to update: ${calculateCal}</label>`)
+    $(".updateModal2").append(`<label class="carbsToUpdate">Carbs to update: ${calculateCar}</label>`)
+    $(".updateModal2").append(`<label class="proteinToUpdate">Protein to update: ${calculatePro}</label>`)
+    $(".updateModal2").append(`<button class="finalUpdate success button" data-close type="button">Submit Changes!</button>`)
+    
+    var updatedCal = parseInt($(".bar1").attr(`aria-valuenow`)) + parseInt(calculateCal)
+    $(".bar1").attr(`aria-valuenow`, updatedCal)
+    var updatedCar = parseInt($(".bar2").attr(`aria-valuenow`)) + parseInt(calculateCar)
+    $(".bar2").attr(`aria-valuenow`, updatedCar)
+    var updatedPro = parseInt($(".bar3").attr(`aria-valuenow`)) + parseInt(calculatePro)
+    $(".bar3").attr(`aria-valuenow`, updatedPro)
+    progressBars()
+})
+
+$(".recipeUpdateBtn3").on("click", function() {
+    var servingsEaten = parseInt($(".servingsEaten3").val())
+    var pullCal3 = $(".2a").attr("calNum2")
+    var calculateCal = (servingsEaten * pullCal3)
+    var pullCar3 = $(".2b").attr("carNum2")
+    var calculateCar = (servingsEaten * pullCar3)
+    var pullPro3 = $(".2c").attr("proNum2")
+    var calculatePro = (servingsEaten * pullPro3)
+    $(".updateModal3").empty()
+    $(".updateModal3").append(`<label class="caloriesToUpdate">Calories to update: ${calculateCal}</label>`)
+    $(".updateModal3").append(`<label class="carbsToUpdate">Carbs to update: ${calculateCar}</label>`)
+    $(".updateModal3").append(`<label class="proteinToUpdate">Protein to update: ${calculatePro}</label>`)
+    $(".updateModal3").append(`<button class="finalUpdate success button" data-close type="button">Submit Changes!</button>`)
+    
+    var updatedCal = parseInt($(".bar1").attr(`aria-valuenow`)) + parseInt(calculateCal)
+    $(".bar1").attr(`aria-valuenow`, updatedCal)
+    var updatedCar = parseInt($(".bar2").attr(`aria-valuenow`)) + parseInt(calculateCar)
+    $(".bar2").attr(`aria-valuenow`, updatedCar)
+    var updatedPro = parseInt($(".bar3").attr(`aria-valuenow`)) + parseInt(calculatePro)
+    $(".bar3").attr(`aria-valuenow`, updatedPro)
+    progressBars()
+})
+
+$(".recipeUpdateBtn4").on("click", function() {
+    var servingsEaten = parseInt($(".servingsEaten4").val())
+    var pullCal4 = $(".3a").attr("calNum3")
+    var calculateCal = (servingsEaten * pullCal4)
+    var pullCar4 = $(".3b").attr("carNum3")
+    var calculateCar = (servingsEaten * pullCar4)
+    var pullPro4 = $(".3c").attr("proNum3")
+    var calculatePro = (servingsEaten * pullPro4)
+    $(".updateModal4").empty()
+    $(".updateModal4").append(`<label class="caloriesToUpdate">Calories to update: ${calculateCal}</label>`)
+    $(".updateModal4").append(`<label class="carbsToUpdate">Carbs to update: ${calculateCar}</label>`)
+    $(".updateModal4").append(`<label class="proteinToUpdate">Protein to update: ${calculatePro}</label>`)
+    $(".updateModal4").append(`<button class="finalUpdate success button" data-close type="button">Submit Changes!</button>`)
+    
+    var updatedCal = parseInt($(".bar1").attr(`aria-valuenow`)) + parseInt(calculateCal)
+    $(".bar1").attr(`aria-valuenow`, updatedCal)
+    var updatedCar = parseInt($(".bar2").attr(`aria-valuenow`)) + parseInt(calculateCar)
+    $(".bar2").attr(`aria-valuenow`, updatedCar)
+    var updatedPro = parseInt($(".bar3").attr(`aria-valuenow`)) + parseInt(calculatePro)
+    $(".bar3").attr(`aria-valuenow`, updatedPro)
+    progressBars()
+})
+
+$(".recipeUpdateBtn5").on("click", function() {
+    var servingsEaten = parseInt($(".servingsEaten5").val())
+    var pullCal5 = $(".4a").attr("calNum4")
+    var calculateCal = (servingsEaten * pullCal5)
+    var pullCar5 = $(".4b").attr("carNum4")
+    var calculateCar = (servingsEaten * pullCar5)
+    var pullPro5 = $(".4c").attr("proNum4")
+    var calculatePro = (servingsEaten * pullPro5)
+    $(".updateModal5").empty()
+    $(".updateModal5").append(`<label class="caloriesToUpdate">Calories to update: ${calculateCal}</label>`)
+    $(".updateModal5").append(`<label class="carbsToUpdate">Carbs to update: ${calculateCar}</label>`)
+    $(".updateModal5").append(`<label class="proteinToUpdate">Protein to update: ${calculatePro}</label>`)
+    $(".updateModal5").append(`<button class="finalUpdate success button" data-close type="button">Submit Changes!</button>`)
+    
+    var updatedCal = parseInt($(".bar1").attr(`aria-valuenow`)) + parseInt(calculateCal)
+    $(".bar1").attr(`aria-valuenow`, updatedCal)
+    var updatedCar = parseInt($(".bar2").attr(`aria-valuenow`)) + parseInt(calculateCar)
+    $(".bar2").attr(`aria-valuenow`, updatedCar)
+    var updatedPro = parseInt($(".bar3").attr(`aria-valuenow`)) + parseInt(calculatePro)
+    $(".bar3").attr(`aria-valuenow`, updatedPro)
+    progressBars()
+})
+
 
 
